cc5b67e0c6e0d4c9ec9e2474730e429752a044c5
Bl Bltich <vissar2016@yandex.ru>
2023-11-06 01:29:03 +0300

    feat: user and film models added

---
+++ models/user.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import validate from "validator";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator(v) {
+                return validate.isEmail(v);
+            },
+            message: (props) => `Укажите корректный адрес электронной почты: ${props.value}.`
+        },
+    },
+    password: {
+        type: String,
+        required: {
+            value: true,
+            message: 'Поле password является обязательнымю.'
+        },
+        select: false,
+    },
+    name: {
+        type: String,
+        required: {
+            value: true,
+            message: 'Поле name является обящательным.'
+        },
+        minlength: 2,
+        maxlength: 30,
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+export default User;
