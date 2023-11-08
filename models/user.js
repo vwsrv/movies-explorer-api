f440a34d0b02d48f2fa519c23e35eaa418327d64
Bl Bltich <vissar2016@yandex.ru>
2023-11-08 20:59:06 +0300

    fix: added unique param to email value

--- models/user.js
+++ models/user.js
@@ -1,35 +1,36 @@
-import mongoose from "mongoose";
-import validate from "validator";
+import mongoose from 'mongoose';
+import validate from 'validator';
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        validate: {
-            validator(v) {
-                return validate.isEmail(v);
-            },
-            message: (props) => `Укажите корректный адрес электронной почты: ${props.value}.`
-        },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator(v) {
+        return validate.isEmail(v);
+      },
+      message: (props) => `Укажите корректный адрес электронной почты: ${props.value}.`,
     },
-    password: {
-        type: String,
-        required: {
-            value: true,
-            message: 'Поле password является обязательнымю.'
-        },
-        select: false,
+  },
+  password: {
+    type: String,
+    required: {
+      value: true,
+      message: 'Поле password является обязательнымю.',
     },
-    name: {
-        type: String,
-        required: {
-            value: true,
-            message: 'Поле name является обящательным.'
-        },
-        minlength: 2,
-        maxlength: 30,
-    }
-});
+    select: false,
+  },
+  name: {
+    type: String,
+    required: {
+      value: true,
+      message: 'Поле name является обящательным.',
+    },
+    minlength: 2,
+    maxlength: 30,
+  },
+}, { versionKey: false, timestamps: true });
 
 const User = mongoose.model('user', userSchema);
 export default User;
