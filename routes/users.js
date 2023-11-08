4f63032272336f7331c94daa6d1b2db269bc7057
Bl Bltich <vissar2016@yandex.ru>
2023-11-08 22:11:05 +0300

    fix: movies and users routes fixed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getCurrentUserInfo, updateUserInfo } from '../controllers/users';
+import { updateUserInfoValidationSchema } from '../validations/users';
 
 const usersRouter = Router();
 
-usersRouter.get('/me', getCurrentUserInfo);
-usersRouter.patch('users/me', updateUserInfo);
+usersRouter.get('/users/me', getCurrentUserInfo);
+usersRouter.patch('/users/me', updateUserInfoValidationSchema, updateUserInfo);
 
 export default usersRouter;
